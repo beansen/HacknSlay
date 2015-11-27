efc27b6b8f58e43db59701e8a7921f836141c9f3
beansen <beansen77@freenet.de>
2015-11-27 22:28:53 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 3;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * horizontal * (Time.deltaTime * speed));
+        transform.Translate(Vector3.forward * vertical * (Time.deltaTime * speed));
+
+        if (Input.GetKeyDown("joystick button 0"))
+        {
+            Debug.Log("Button 0");
+        }
+
+        if (Input.GetKeyDown("joystick button 1"))
+        {
+            Debug.Log("Button 1");
+        }
+
+        if (Input.GetKeyDown("joystick button 2"))
+        {
+            Debug.Log("Button 2");
+        }
+
+        if (Input.GetKeyDown("joystick button 3"))
+        {
+            Debug.Log("Button 3");
+        }
+    }
+}
